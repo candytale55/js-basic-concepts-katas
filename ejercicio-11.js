02f6593270f098d2bd75012813746e195f81d7a8
CandyTale55 <candytale55@gmail.com>
2025-11-01 13:46:23 +0100

    add ejercicio-11 - make it more readable

--- ejercicio-11.js
+++ ejercicio-11.js
@@ -5,9 +5,10 @@ y de lo contrario cuente la longitud del string y lo sume. */
 const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
 
 function averageWord(mixedArray) {
-    const numberArray = mixedArray.map(element => typeof element === 'string' ? element.length : element);
+    const numberArray = mixedArray.map(element =>
+        typeof element === 'string' ? element.length : element);
     // console.log(numberArray);
-    return (numberArray.reduce((sum, current)=> sum + current, 0) / numberArray.length).toFixed(2) ;
+    return (numberArray.reduce((sum, current)=> sum + current, 0) / numberArray.length).toFixed(2);
 }
 
 console.log(averageWord(mixedElements));
