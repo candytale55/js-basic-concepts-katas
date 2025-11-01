bc52934534d3fc387de44e6e87b093fbed969dc4
CandyTale55 <candytale55@gmail.com>
2025-11-01 13:42:39 +0100

    add ejercicio-11 - complete

---
+++ ejercicio-11.js
@@ -0,0 +1,13 @@
+/* Calcular promedio de strings: Crea una función que reciba 
+por parámetro un array y cuando es un valor number lo sume 
+y de lo contrario cuente la longitud del string y lo sume. */
+
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+
+function averageWord(mixedArray) {
+    const numberArray = mixedArray.map(element => typeof element === 'string' ? element.length : element);
+    // console.log(numberArray);
+    return (numberArray.reduce((sum, current)=> sum + current, 0) / numberArray.length).toFixed(2) ;
+}
+
+console.log(averageWord(mixedElements));
