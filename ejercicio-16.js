19c8453907d451fd1fdd03dc493533dd9a93000d
CandyTale55 <candytale55@gmail.com>
2025-11-02 15:32:55 +0100

    add ejercicio-16 - Complete

---
+++ ejercicio-16.js
@@ -0,0 +1,9 @@
+/* Probando For...of: 
+    Usa un bucle for of para recorrer todos los destinos del array. 
+    Imprime en un console.log sus valores */
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (place of placesToTravel) {
+    console.log(place);
+}
